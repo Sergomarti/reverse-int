f6881b495498186c9b22031430e9ebb93f7b9e41
Sergomarti <61779959+Sergomarti@users.noreply.github.com>
2020-10-12 18:20:33 +0300

    fix: fixed the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    var digit, result = 0
+
+    while( n ){
+        digit = n % 10                  //  Get last digit. Ex. 123/10 → 12.3 → 3
+        result = (result * 10) + digit  //  Ex. 123 → 1230 + 4 → 1234
+        n = n/10|0                      //  Remove last digit. Ex. 123 → 12.3 → 12
+    }
+    var finish = Math.abs(result)
   
+    return finish
 }
